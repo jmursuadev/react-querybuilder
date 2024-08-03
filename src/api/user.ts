c2eaad8dbcf4154962091131476bef0e538f5ab7
Jonathan Ursua <>
2024-08-03 23:36:33 +0800

    use env variable

--- src/api/user.ts
+++ src/api/user.ts
@@ -2,7 +2,7 @@ export const fetchUsers = async (queryParams: string = '') => {
   /**
    * TODO: use env variable for the server URL
    */
-  const response = await fetch('http://localhost:3001/users?' + queryParams, {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_POSTGREST_URL}/users?${queryParams}`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
